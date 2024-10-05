c37b43c8f9b08954ba44592e8e5752020e4f47ea
davidwallacem1982 <davidwallacem@hotmail.com>
2024-10-05 18:34:58 -0300

    @davidwallacem1982 - Melhoria nos testes de geração de Gravatar URL e validação de e-mail;

--- Gravatar.Tests/GravatarExtensionTests.cs
+++ Gravatar.Tests/GravatarExtensionTests.cs
@@ -5,9 +5,8 @@ namespace Gravatar.Tests
     [TestClass]
     public class GravatarExtensionTests
     {
-        //https://www.gravatar.com/avatar/a042db3d80d46a527f8ba1605a74be57
         [TestCategory("Gravatar Tests")]
-        [TestMethod(displayName:"Should Validate Gravatar Extension")]
+        [TestMethod(displayName: "Should Validate Gravatar Extension")]
         [DataRow(null, 200, false)]
         [DataRow("", 200, false)]
         [DataRow(" ", 200, false)]
@@ -15,13 +14,12 @@ namespace Gravatar.Tests
         [DataRow("d@david.com", 200, false)]
         [DataRow("davidwallacem@hotmail.com", null, true)]
         [DataRow("davidwallacem@hotmail.com", 200, true)]
-        //[DataRow("https://www.gravatar.com/avatar/a042db3d80d46a527f8ba1605a74be57")]
         public void ShouldValidateGravatar(string email, int? size, bool status)
         {
-            var imagemSize = size.HasValue ? size.Value : 50;
-            var result = $"https://www.gravatar.com/avatar/a042db3d80d46a527f8ba1605a74be57?s={imagemSize}";
+            var imagemSize = size ?? 50;
+            var result = email?.ToGravatar(imagemSize) ?? string.Empty;
 
-            Assert.AreEqual((email.ToGravatar(size ?? 50) == result), status);
+            Assert.AreEqual(result == $"https://www.gravatar.com/avatar/a042db3d80d46a527f8ba1605a74be57?s={imagemSize}", status);
         }
     }
 }
